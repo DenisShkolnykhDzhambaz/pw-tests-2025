affce5467fefa1a65c2233cde10b683d9f5f3b5c
DenisShkolnykhDzhambaz <dzhambaz369@gmail.com>
2025-06-05 19:47:04 +0300

    add 05-group-and-hooks.test.ts and add modified 03-light-dark-mode-switching.test.ts

--- tests/03-light-dark-mode-switching.test.ts
+++ tests/03-light-dark-mode-switching.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Switch between light and dark mode assertion', async({ page }) => {
-  page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/');
   await expect(page.locator('html')).toHaveAttribute('data-theme-choice', 'system');
   await page.getByRole('button', { name: 'Switch between dark and light' }).click();
   await expect(page.locator('html')).toHaveAttribute('data-theme', 'light');
@@ -10,7 +10,7 @@ test('Switch between light and dark mode assertion', async({ page }) => {
 });
 
 test('Switch between light and dark mode assertion, ver.2', async({ page }) => {
-  page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/');
   const locator = page.locator('button[class="clean-btn toggleButton_gllP"]');
   await expect(locator).toHaveAttribute('aria-label', 'Switch between dark and light mode (currently system mode)');
   await page.click('button[class="clean-btn toggleButton_gllP"]');
